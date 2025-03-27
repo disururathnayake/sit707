81672f9e4a4ae3255276797f98b9adb298719bc4
disururathnayake <disuru.office@gmail.com>
2025-03-27 21:21:13 +1100

    first commit

---
+++ selenium/src/main/java/sit707/Main.java
@@ -0,0 +1,32 @@
+
+
+package sit707;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", 
+							"C:/Users/Disuru/Downloads/chromedriver-win64/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		System.out.println(driver);
+		
+		driver.get("https://www.google.com");
+		
+		try {
+			Thread.sleep(5 * 1000);
+			
+		}catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		driver.close();
+	}
+
+}
+
